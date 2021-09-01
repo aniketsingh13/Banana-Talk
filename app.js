fc459e03ec2ef6efdee4a5be828a5b8124dc19cb
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-09-01 09:05:37 -0700

    html and css done

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 btnTranslate.addEventListener("click",  clickHandler);
+function clickHandler (){
+    console.log("input",txtInput.value);
+}
