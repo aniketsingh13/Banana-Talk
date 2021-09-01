ec0c54d65c5191b1f5306f27861ba82fd0dfadea
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-09-01 11:03:35 -0700

    add more code in the js,html and css

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-btnTranslate.addEventListener("click",  clickHandler);
-function clickHandler (){
-    console.log("input",txtInput.value);
+var txtOutput = document.querySelector("#txt-output");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationUrl(input) {
+    return serverUrl + "?" + "text=" + input
 }
+
+function errorHandler(error) {
+    alert("something wrong with the server!,plese try after sometimes")
+}
+
+function clickHandler() {
+    var inputText = txtInput.Value;
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
