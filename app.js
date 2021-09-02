abdfb90f2b6ee4f933058753a0e7d44a87d487df
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-09-02 00:14:42 -0700

    update app.js

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
 
-function getTranslationUrl(text) {
+function getTranslationURL(text) {
     return serverURL  + "?" + "text=" + text ;
 }
 
 function errorHandler(error) {
-    alert("something wrong with the server!,plese try after sometimes")
+    alert("something wrong with the server!,plese try after sometimes");
 }
 
 function clickHandler() {
     var inputText = txtInput.Value;
-    fetch (getTranslationUrl(inputText))
+    fetch (getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
